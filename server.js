ea465f58be89ed8a078dddc9de770edafaf89e41
Manish Mittal <manish.mittal@suffescom.com>
2023-12-29 12:17:11 +0530

    vercel file added

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
-const multer = require("multer");
 require("dotenv").config();
 const { initializeApp } = require("firebase/app");
 const { firebaseConfig } = require("./firebaseConfig");
-const { fileUpload } = require("./controllers/uploadFile");
-const { validateToken } = require("./controllers/validateToken");
-const { sendMail } = require("./controllers/nodemailer");
+// const { sendMail } = require("./controllers/nodemailer");
 const { addCountry, countryList } = require("./controllers/country");
 const { addCity, cityList } = require("./controllers/city");
 
@@ -16,19 +13,15 @@ app.use(cors());
 app.use(express.json());
 
 const firebaseapp = initializeApp(firebaseConfig);
-const upload = multer({ storage: multer.memoryStorage() });
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
-app.post("/upload", upload.single("image"), fileUpload);
-app.post("/validateToken", validateToken);
+
 app.post("/addCountry", addCountry);
 app.post("/countryList", countryList);
 app.post("/addCity", addCity);
 app.post("/cityList", cityList);
-
-app.post("/sendMail", sendMail);
 
 mongoose
   .connect(process.env.DB_URL, {
